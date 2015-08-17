c3d8de588cf1998f64c97cfc7a0d11aeea03b55f
Chris Lytsell & Samantha Maturen <student@epicodus.com>
2015-08-17 16:23:33 -0700

    modify twig search page so it renders...and get project to work!

--- app/app.php
+++ app/app.php
@@ -36,15 +36,15 @@
     // search for task function
     $app->get("/task_search_results", function() use ($app){
         $task_to_search = $_GET['search_name'];
-        if (strlen($task_to_search) > 0){
-            //change Task::find() to return array
-            //=================================
-            $found_tasks = (Task::find($task_to_search));
-            var_dump($found_tasks);
+        // if (strlen($task_to_search) > 0){
+        //     //change Task::find() to return array
+        //     //=================================
+             $found_tasks = (Task::find($task_to_search));
+            // var_dump($found_tasks);
             return $app['twig']->render('task_search_results.html.twig', array('tasks' => $found_tasks));
-        } else{
-            return $app['twig']->render('error.html.twig');
-        }
+        // } else{
+        //     return $app['twig']->render('error.html.twig');
+        // }
     });
 
 
