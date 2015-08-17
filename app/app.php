2bd1e330960cf3d2b6a0d29914774721e354d49f
Chris Lytsell & Samantha Maturen <student@epicodus.com>
2015-08-17 14:40:28 -0700

    Add delete pages for tasks and categories, changed functions in app.php to post.

--- app/app.php
+++ app/app.php
@@ -19,26 +19,38 @@
         return $app['twig']->render('index.html.twig');
     });
 
-    $app->post("/tasks", function() use ($app)
+    $app->get("/tasks", function() use ($app)
     {
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-    $app->get("/categories", function() use ($app) {
-        return $app['twig']->render('categories.html.twig', array('categories' => Category:getAll()));
-    });
-
     $app->post("/tasks", function() use ($app){
-        $task = new Task($_POST['description']);
+        $task = new Task($_POST['name']);
         $task->save();
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-    $app->post("/delete_tasks", function() use ($app)
-    {
+    $app->post("/delete_tasks", function() use ($app){
         Task::deleteAll();
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('delete_tasks.html.twig');
     });
+
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/categories", function() use ($app){
+        $category = new Category($_POST['name']);
+        $category->save();
+        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app){
+        Category::deleteAll();
+        return $app['twig']->render('delete_categories.html.twig');
+    });
+
+
 
     return $app;
 ?>
