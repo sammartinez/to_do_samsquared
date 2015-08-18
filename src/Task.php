0341336025c2812bc25810a9a754ba57ab54bf2a
samantha maturen & Deron Johnson <student@epicodus.com>
2015-08-18 10:15:57 -0700

    Change Task methods to handle category ids

--- src/Task.php
+++ src/Task.php
@@ -2,12 +2,14 @@
 class Task
 {
     private $description;
+    private $category_id;
     private $id;
 
-    function __construct($description, $id = null)
+    function __construct($description, $id = null, $category_id)
     {
         $this->description = $description;
         $this->id = $id;
+        $this->category_id = $category_id;
     }
 
     function setDescription($new_description)
@@ -25,24 +27,15 @@ class Task
         return $this->id;
     }
 
+    function getCategoryId()
+    {
+        return $this->category_id;
+    }
 
     function save()
     {
-        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}', {$this->category_id()})");
         $this->id = $GLOBALS['DB']->lastInsertId();
-    }
-
-    static function find($search_name)
-    {
-        $found_tasks = array();
-        $tasks = Task::getAll();
-        foreach($tasks as $task){
-            //$task_name = $task->getDescription();
-            if ($task->getDescription() == $search_name){
-                array_push($found_tasks, $task->getDescription());
-            }
-        }
-        return $found_tasks;
     }
 
     static function getAll()
@@ -52,8 +45,9 @@ class Task
         foreach($returned_tasks as $task){
             $description = $task['description'];
             $id = $task['id'];
+            $category_id = $task['category_id'];
             //var_dump($id);
-            $new_task = new Task($description, $id);
+            $new_task = new Task($description, $id, $category_id);
             array_push($tasks, $new_task);
         }
         return $tasks;
@@ -63,6 +57,20 @@ class Task
     {
         $GLOBALS['DB']->exec("DELETE FROM tasks;");
     }
+
+    static function find($search_name)
+    {
+        $found_tasks = array();
+        $tasks = Task::getAll();
+        foreach($tasks as $task){
+            if ($task->getDescription() == $search_name){
+                array_push($found_tasks, $task->getDescription());
+            }
+        }
+        return $found_tasks;
+    }
+
+
 }
 
 ?>
