dd0870e831a3d0ccb4f101f95a30f8567f08a069
samantha maturen & Deron Johnson <student@epicodus.com>
2015-08-18 11:50:52 -0700

    Add due_date to twig templates

--- app/app.php
+++ app/app.php
@@ -29,7 +29,8 @@
     $app->post("/tasks", function() use ($app){
         $description = $_POST['description'];
         $category_id = $_POST['category_id'];
-        $task = new Task($description, $id = null, $category_id);
+        $due_date = $_POST['due_date'];
+        $task = new Task($description, $id = null, $category_id, $due_date);
         $task->save();
         $category = Category::find($category_id);
         return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => Task::getAll()));
